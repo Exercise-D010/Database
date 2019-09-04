05b9d3801cafc331fec123012345db7afd619571
Giang <iservice91@gmail.com>
2019-09-04 22:17:30 +0700

    create page profile

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './pages/login/login';
+import ProfilePage from './pages/profile/profile';
 
 function Aaa() {
   return (
     <div className="App">
       <LoginPage></LoginPage>
+      <ProfilePage></ProfilePage>
     </div>
   );
 }
