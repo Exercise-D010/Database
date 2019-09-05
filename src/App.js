9091eee11c57e275aaa7aa1ebb439906cf929224
Giang <iservice91@gmail.com>
2019-09-05 21:48:53 +0700

    create page dashboard

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import LoginPage from './pages/login/login';
 import ProfilePage from './pages/profile/profile';
+import DashboardPage from './pages/dashboard/dashboard';
 
 function Aaa() {
   return (
     <div className="App">
       <LoginPage></LoginPage>
       <ProfilePage></ProfilePage>
+      <DashboardPage></DashboardPage>
     </div>
   );
 }
