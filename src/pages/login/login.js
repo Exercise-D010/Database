ba49832d6d6f28676cb469e1e1c706f607d16127
Giang <iservice91@gmail.com>
2019-09-11 21:07:11 +0700

    chinh sua login 04

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -7,22 +7,30 @@ import Logo from '../../assets/images/logo.png';
 
 class LoginPage extends React.Component{
     render(){
-        return(
-            <div>
-                <div className="login">
-                    <div className="login-box">
-                        <img src={Logo} alt="hinh logo" className="logo"/>
+        return(     
+            <div className="login">
+                <div className="body-login">
+                    <div className="circle">
                        
-                        <form className="khung">
-                            <input type="text" name="username" placeholder="username" className="form-control"/>
-                            <input type="password" name="password" placeholder="password" className="form-control"/>
-                            <input type="checkbox" name="ghinho"/> Remember me
-                            <input type="submit" name="submit" value="Login" className="btn btn-danger form-control"/>
-                            <p>Don't have an account? <a href="#">Sign Up</a></p>
-                            <p><a href="#">Forgot your password?</a></p>
-                        </form>
-                        
                     </div>
+                    
+                    <form className="form">
+                        <div className='form-group'>
+                            <input type="text" name="username" placeholder="username" className="form-control"/>
+                        </div>
+                        <div className='form-group'>
+                                <input type="password" name="password" placeholder="password" className="form-control"/>    
+                        </div>
+                        <div className='form-group form-check'>
+                                <input type="checkbox" name="remember"/> Remember me
+                        </div>
+                        <div className='form-group'>
+                            <input type="submit" name="submit" value="Login" className="btn btn-danger form-control"/>  
+                        </div>
+                        <p>Don't have an account? <a href="#">Sign Up</a></p>
+                        <p><a href="#">Forgot your password?</a></p>
+                    </form>
+                    
                 </div>
             </div>
         )
