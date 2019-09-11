a78f7b35be34a9a36c867e70fc5c15b0ac02f7d8
Giang <iservice91@gmail.com>
2019-09-11 18:17:07 +0700

    chinh sua login 03

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -13,12 +13,15 @@ class LoginPage extends React.Component{
                     <div className="login-box">
                         <img src={Logo} alt="hinh logo" className="logo"/>
                        
-                        <i>Username</i>
-                        <input type="text" name="username" placeholder="Enter Username"/>
-                        <i>Password</i>
-                        <input type="password" name="password" placeholder="Enter Password"/>
-                        <input type="submit" name="submit" value="Login"/>
-                        <a href="#">Forget Password</a>
+                        <form className="khung">
+                            <input type="text" name="username" placeholder="username" className="form-control"/>
+                            <input type="password" name="password" placeholder="password" className="form-control"/>
+                            <input type="checkbox" name="ghinho"/> Remember me
+                            <input type="submit" name="submit" value="Login" className="btn btn-danger form-control"/>
+                            <p>Don't have an account? <a href="#">Sign Up</a></p>
+                            <p><a href="#">Forgot your password?</a></p>
+                        </form>
+                        
                     </div>
                 </div>
             </div>
