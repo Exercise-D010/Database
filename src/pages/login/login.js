d6876aa086872a1edba7ade9128bbc6242777c58
Giang <iservice91@gmail.com>
2019-09-11 23:01:46 +0700

    chinh sua login 05

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import './login.scss';
-import Logo from '../../assets/images/logo.png';
-
-
 
 
 class LoginPage extends React.Component{
@@ -13,25 +10,8 @@ class LoginPage extends React.Component{
                     <div className="circle">
                        
                     </div>
-                    
-                    <form className="form">
-                        <div className='form-group'>
-                            <input type="text" name="username" placeholder="username" className="form-control"/>
-                        </div>
-                        <div className='form-group'>
-                                <input type="password" name="password" placeholder="password" className="form-control"/>    
-                        </div>
-                        <div className='form-group form-check'>
-                                <input type="checkbox" name="remember"/> Remember me
-                        </div>
-                        <div className='form-group'>
-                            <input type="submit" name="submit" value="Login" className="btn btn-danger form-control"/>  
-                        </div>
-                        <p>Don't have an account? <a href="#">Sign Up</a></p>
-                        <p><a href="#">Forgot your password?</a></p>
-                    </form>
-                    
                 </div>
+                
             </div>
         )
     }
