b4e16e08c776b3bf06f242ea214adb3b41f7475f
giang <iservice91@gmail.com>
2019-09-21 22:38:37 +0700

    tao file dang xuat

--- src/components/menu-bar/menu-bar.js
+++ src/components/menu-bar/menu-bar.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './menu-bar.scss';
 import { Navbar, Nav, NavDropdown, Form, Button, FormControl } from 'react-bootstrap';
+import Cookies from 'universal-cookie';
+import { Redirect } from 'react-router-dom';
 
 class MenuBar extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            logouted: false
+        }
+    }
+
+    onClick = (event) => {
+        console.log(event)
+        let cookie = new Cookies()
+        cookie.set("isLogin", "false")
+        this.setState({
+            logouted: cookie.get("isLogin") === "false"
+        })
+    }
+
+    renderDirectTo(){
+        if (this.state.logouted){
+            return <Redirect to="/login"></Redirect>
+        }
+    }
+
     render(){
         return(
             <React.Fragment>
+                {this.renderDirectTo()}
                 <Navbar bg="light" expand="lg">
                     <Navbar.Brand href="#home">Green Adecamy</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -27,8 +52,10 @@ class MenuBar extends React.Component{
                        
                         <Form inline >
                         <FormControl type="text" placeholder="Tìm Kiếm" className="mr-sm-2" />
-                        <Button variant="outline-success">Tìm Kiếm</Button>
-                        </Form>                        
+                        <Button variant="outline-success" className="mr-sm-2" >Tìm Kiếm</Button>
+                        <Button variant="outline-dark" onClick={(e) => this.onClick(e)}>Đăng Xuất</Button>   
+                        </Form> 
+                                          
                     </Navbar.Collapse>
                 </Navbar>
             </React.Fragment>
