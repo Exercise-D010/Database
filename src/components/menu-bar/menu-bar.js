5b1d16916d13bb8eb2d314e3742038ea8e291d1c
giang <iservice91@gmail.com>
2019-09-23 20:59:50 +0700

    Chuyen Ngon Ngu

--- src/components/menu-bar/menu-bar.js
+++ src/components/menu-bar/menu-bar.js
@@ -1,59 +1,53 @@
 import React from 'react';
 import './menu-bar.scss';
 import { Navbar, Nav, NavDropdown, Form, Button, FormControl } from 'react-bootstrap';
-import Cookies from 'universal-cookie';
-import { Redirect } from 'react-router-dom';
+import {locale} from '../../config/locale'
+
 
 class MenuBar extends React.Component{
-    constructor(){
+    constructor() {
         super()
-        this.state={
-            logouted: false
+        this.state ={
+           lang: "vi" 
         }
+        locale.setLanguage(this.state.lang)   
     }
 
-    onClick = (event) => {
-        console.log(event)
-        let cookie = new Cookies()
-        cookie.set("isLogin", "false")
+    switchLanguage = () =>{
         this.setState({
-            logouted: cookie.get("isLogin") === "false"
+            lang: this.state.lang == "vi" ? "en": "vi"
         })
-    }
-
-    renderDirectTo(){
-        if (this.state.logouted){
-            return <Redirect to="/login"></Redirect>
-        }
+        locale.setLanguage(this.state.lang)  
+        this.setState({})
     }
 
     render(){
         return(
-            <React.Fragment>
-                {this.renderDirectTo()}
+            <React.Fragment>    
                 <Navbar bg="light" expand="lg">
                     <Navbar.Brand href="#home">Green Adecamy</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="/home">Trang Chủ</Nav.Link>                   
-                            <NavDropdown title="Khóa Đào Tạo" id="basic-nav-dropdown" href="/khoa-dao-tao">
+                            <Nav.Link href="/home">{ locale.home }</Nav.Link>                   
+                            <NavDropdown title={ locale.trainning } id="basic-nav-dropdown" href="/khoa-dao-tao">
                                 <NavDropdown.Item href="#action/3.1">Thiết kế Web</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Thiết kế App</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Thiết kế nội thất</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">Tiếng Hàn</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href="/blog">Blog</Nav.Link>
-                            <Nav.Link href="/gioi-thieu">Giới Thiệu</Nav.Link>
-                            <Nav.Link href="/tuyen-dung">Tuyển Dụng</Nav.Link>
-                            <Nav.Link href="/lien-he">Liên Hệ</Nav.Link>
+                            <Nav.Link href="/blog">{ locale.blog }</Nav.Link>
+                            <Nav.Link href="/gioi-thieu">{ locale.about }</Nav.Link>
+                            <Nav.Link href="/tuyen-dung">{ locale.hire }</Nav.Link>
+                            <Nav.Link href="/lien-he">{ locale.contact }</Nav.Link>
                         </Nav>
                        
                         <Form inline >
-                        <FormControl type="text" placeholder="Tìm Kiếm" className="mr-sm-2" />
-                        <Button variant="outline-success" className="mr-sm-2" >Tìm Kiếm</Button>
-                        <Button variant="outline-dark" onClick={(e) => this.onClick(e)}>Đăng Xuất</Button>   
+                        <FormControl type="text" placeholder={ locale.search } className="mr-sm-2" />
+                        <Button variant="outline-success" className="mr-sm-2" >{ locale.search }</Button>
+                        <Button variant="outline-dark" className="mr-sm-2">{ locale.login }</Button>
+                        <Button variant="outline-danger" className="mr-sm-2" onClick={() => this.switchLanguage()} >{ locale.lang }</Button>
                         </Form> 
                                           
                     </Navbar.Collapse>
---
+++ src/config/locale.js
@@ -0,0 +1,36 @@
+import LocalizedStrings from 'react-localization';
+
+export const locale = new LocalizedStrings(
+   {
+        vi: {
+            lang: "Tiếng Việt",
+            home: "Trang Chủ",
+            trainning: "Khóa Đào Tạo",
+            blog: "Blog",
+            about: "Giới Thiệu",
+            hire: "Tuyển Dụng",
+            contact: "Liên Hệ",
+            search: "Tìm Kiếm",
+            login: "Đăng Nhập",
+            logout: "Đăng Xuất",
+            phone_number: "Số Điện Thoại",
+            password: "Mật Khẩu"
+            
+        },
+
+         en: {
+            lang: "English",
+            home: "Home",
+            trainning: "Trainning",
+            blog: "Blog",
+            about: "About Us",
+            hire: "Hiring",
+            contact: "Contact",
+            search: "Search",
+            login: "Login",
+            logout: "Logout",
+            phone_number: "Phone Number",
+            password: "Password"
+        }
+   } 
+)
