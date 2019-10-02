83d182c6fc614c9deab7e31e418280ae19ec8186
giang <iservice91@gmail.com>
2019-10-02 21:17:33 +0700

    chuyen slide qua header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import './header.scss';
+import { Slide } from 'react-slideshow-image';
 
+const slideImages = [
+    'https://picsum.photos/id/1018/1920/500' ,
+    'https://picsum.photos/id/1020/1920/500' ,
+    'https://picsum.photos/id/898/1400/500',
+    'https://picsum.photos/id/1006/1920/500',
+    'https://picsum.photos/id/1015/1920/500',
+  ];
+  
+  const properties = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    arrows: true,
+    onChange: (oldIndex, newIndex) => {
+      console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+    }
+  }
 
 class HeaderElement extends React.Component{
     render(){
         return(
-            <div className="headerpage">
-               
-            </div>
+           <React.Fragment>
+               <div className="slide-container">
+                    <Slide {...properties}>
+                    {
+                        slideImages.map(
+                            (v,i)=>(
+                                <div className="each-slide">
+                                    <div style={{'backgroundImage': `url(${slideImages[i]})`}}>
+                                    </div>
+                                </div>  
+                            )
+                        )
+                    }
+                    </Slide>
+                </div>  
+           </React.Fragment>
         )
     }
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,46 +1,15 @@
 import React from 'react';
 import './home.scss';
-import { Slide } from 'react-slideshow-image';
+import HeaderElement from '../../components/header/header';
 
-const slideImages = [
-    'https://picsum.photos/id/1018/1920/500' ,
-    'https://picsum.photos/id/1020/1920/500' ,
-    'https://picsum.photos/id/898/1400/500',
-    'https://picsum.photos/id/1006/1920/500',
-    'https://picsum.photos/id/1015/1920/500',
-  ];
-  
-  const properties = {
-    duration: 5000,
-    transitionDuration: 500,
-    infinite: true,
-    indicators: true,
-    arrows: true,
-    onChange: (oldIndex, newIndex) => {
-      console.log(`slide transition from ${oldIndex} to ${newIndex}`);
-    }
-  }
+
 
 class HomePage extends React.Component{
     render(){
         return(
             <React.Fragment>
-              <div className="slide-container">
-                    <Slide {...properties}>
-                    {
-                        slideImages.map(
-                            (v,i)=>(
-                                <div className="each-slide">
-                                    <div style={{'backgroundImage': `url(${slideImages[i]})`}}>
-                                    </div>
-                                </div>  
-                            )
-                        )
-                    }
-                    </Slide>
-                </div>  
-            </React.Fragment>
-            
+              
+            </React.Fragment>    
         )
     }
 }
