8aed20fb74caf26623ab26824be5547f0305ca91
giang <iservice91@gmail.com>
2019-10-03 22:37:48 +0700

    xay dung trang tuyen dung

--- src/pages/tuyen-dung/tuyen-dung.js
+++ src/pages/tuyen-dung/tuyen-dung.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './tuyen-dung.scss';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 class TuyendungPage extends React.Component{
     render(){
         return(
             <React.Fragment>
-                 <div>Tuyển Dụng</div>
+                 <h1 className="td">TUYỂN DỤNG</h1>
+                 <Container>
+                   <Row>
+                     <Col>1s</Col>
+                     <Col> 2s</Col>
+                   </Row>
+                 </Container>
             </React.Fragment>
         )
     }
